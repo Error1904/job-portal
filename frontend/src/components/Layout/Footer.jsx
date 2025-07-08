6c437df90c6071fa5fc7e997f506c182265446c4
Darshan Dinesh Mandavkar <darshanmandavkar88@gmail.com>
2025-07-09 00:56:16 +0530

    Updated project

--- frontend/src/components/Layout/Footer.jsx
+++ frontend/src/components/Layout/Footer.jsx
@@ -2,18 +2,18 @@ import React, { useContext } from 'react'
 import {Context} from "../../main"
 import {Link} from "react-router-dom"
 import { FaGithub , FaLinkedin} from "react-icons/fa"
-import { SiLeetcode } from "react-icons/si";
+
 import { RiInstagramFill} from "react-icons/ri"
 function Footer() {
   const {isAuthorized}  = useContext(Context)
   return (
     <footer className= {isAuthorized ? "footerShow" : "footerHide"}>
-<div>&copy; All Rights Reserved by Abhishek.</div>
+<div>&copy; Project by Darshan.</div>
 <div>
-  <Link to={'https://github.com/exclusiveabhi'} target='github'><FaGithub></FaGithub></Link>
-  <Link to={'https://leetcode.com/u/exclusiveabhi/'} target='leetcode'><SiLeetcode></SiLeetcode></Link>
-  <Link to={'https://www.linkedin.com/in/abhishek-rajput-/'} target='linkedin'><FaLinkedin></FaLinkedin></Link>
-  <Link to={'https://www.instagram.com/exclusiveabhi/'} target='instagram'><RiInstagramFill></RiInstagramFill></Link>
+  <Link to={'https://github.com/Error1904'} target='github'><FaGithub></FaGithub></Link>
+  
+  <Link to={'https://www.linkedin.com/in/darshan-mandavkar-0693a0341/'} target='linkedin'><FaLinkedin></FaLinkedin></Link>
+  <Link to={'https://www.instagram.com/19darshan_04/'} target='instagram'><RiInstagramFill></RiInstagramFill></Link>
 </div>
       
     </footer>
